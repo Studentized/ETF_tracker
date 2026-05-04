292d8ff4767c187568cab007bf42c8b94819435d
studentized <studentized@hotmail.com>
2026-05-04 17:29:21 +0100

    Added a list of ETFs in the watch list; Added codes to fetch data of live quotes.

--- tracker.R
+++ tracker.R
@@ -3,12 +3,19 @@ library(quantmod)
 
 # Define the stock symbol (e.g., "AAPL" for Apple or "SPY" for an ETF)
 ticker <- "VUSA.L"
+watchList <- c("VUSA.L", "VUAG.L", "IITU.L", "VUKE.L", "VWRL.L")
 
 # Fetch historical data
 getSymbols(ticker, src = "yahoo", from = "2026-01-01")
+getSymbols(watchList, src = "yahoo", from = "2026-01-01")
 
 # View the first few rows
 head(get(ticker))
+
+for(ticker1 in watchList) {
+  print(paste("Showing data for:", ticker1))
+  print(head(get(ticker1)))
+}
 
 # Create a candlestick chart
 chartSeries(get(ticker),
@@ -17,3 +24,10 @@ chartSeries(get(ticker),
 
 # Add a technical indicator (e.g., a 20-day moving average)
 addSMA(n = 20, col = "blue")
+
+# Get current quotes for the whole list
+current_prices <- getQuote(watchList)
+
+# View the summary table
+print(current_prices)
+
